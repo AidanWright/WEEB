532975bf80d160d79ab581c84ca9bf21dc852f59
AidanWright <38870143+AidanWright@users.noreply.github.com>
2020-02-17 15:36:49 -0500

    Minor Patch
    fixed travis
    changed test script
    linted wipe-dependencies

--- wipe-dependencies.js
+++ wipe-dependencies.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const wipeDependencies = () => {
-  const file = fs.readFileSync('package.json')
-  const content = JSON.parse(file)
+  const file = fs.readFileSync("package.json");
+  const content = JSON.parse(file);
   for (const devDep in content.devDependencies) {
-    content.devDependencies[devDep] = '*'
+    content.devDependencies[devDep] = "*";
   }
-  fs.writeFileSync('package.json', JSON.stringify(content))
-}
+  fs.writeFileSync("package.json", JSON.stringify(content));
+};
 if (require.main === module) {
-  wipeDependencies()
+  wipeDependencies();
 } else {
-  module.exports = wipeDependencies
+  module.exports = wipeDependencies;
 }
